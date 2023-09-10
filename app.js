9db37a8152f0b5c47e9712dc94af9066b3f60dd2
James Oshomah <jamiebones2000@yahoo.co.uk>
2023-09-10 18:41:00 +0100

    added port variable

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const client = new twilio(accountSid, authToken);
 (async () => {
 
     const app = express();
-    const port = 3000;
+    const port = process.env.PORT || 3000;
 
 
     app.use(cors())

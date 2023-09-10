f4d040f74bc441d786431529bf5ed242957985fa
James Oshomah <jamiebones2000@yahoo.co.uk>
2023-09-10 18:58:58 +0100

    changed port

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const client = new twilio(accountSid, authToken);
 (async () => {
 
     const app = express();
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 5000;
 
 
     app.use(cors())
